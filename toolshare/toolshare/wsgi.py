da6fb309aa7be7662d571c196437293267fa689e
R4mTex <poutots@gmail.com>
2023-10-20 11:19:12 +0200

    update requirements.txt for Travis

--- toolshare/toolshare/wsgi.py
+++ toolshare/toolshare/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toolshare.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toolshare.travis')
 
 application = get_wsgi_application()

b0b9aad619efdc5bf87fbf9d7844d202f0bc05fc
R4mTex <poutots@gmail.com>
2023-10-20 14:23:15 +0200

    update requirements.txt for Travis

--- toolshare/toolshare/travis.py
+++ toolshare/toolshare/travis.py
@@ -1,15 +1,15 @@
-from . import *
+from . import settings
 
 # SECURITY WARNING: keep the secret key used in Toolion secret!
-SECRET_KEY="django-insecure-lpxfa$ctf^n0+w7qlbo536ojr7ab!x@k_d#wc80vays9$5^jix"
+settings.SECRET_KEY="django-insecure-lpxfa$ctf^n0+w7qlbo536ojr7ab!x@k_d#wc80vays9$5^jix"
 
 # SECURITY WARNING: don't run with debug turned on in Toolion!
-DEBUG=True
+settings.DEBUG=True
 
 # Reset Password Settings
-EMAIL_HOST="smtp.gmail.com"
-EMAIL_HOST_USER="founder.toolshare@gmail.com"
-EMAIL_HOST_PASSWORD="raaanxhaptucttlm"
+settings.EMAIL_HOST="smtp.gmail.com"
+settings.EMAIL_HOST_USER="founder.toolshare@gmail.com"
+settings.EMAIL_HOST_PASSWORD="raaanxhaptucttlm"
 
 # Google Maps API Key
-GOOGLE_MAPS_API_KEY="AIzaSyD1ckgetM8cnzdgwd1XpEetOhxghe5w82M"
+settings.GOOGLE_MAPS_API_KEY="AIzaSyD1ckgetM8cnzdgwd1XpEetOhxghe5w82M"

3e2c0a01490afd8a6ed31c53dd19f20c6da6b1fd
R4mTex <poutots@gmail.com>
2023-10-31 17:45:55 +0100

    update .circleci/config.yml

--- toolshare/toolshare/wsgi.py
+++ toolshare/toolshare/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from django.conf import settings
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toolshare.settings')
+settings.configure()
 
 application = get_wsgi_application()

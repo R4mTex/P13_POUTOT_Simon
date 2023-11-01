a157d202c29de9e2f8c38c19c6e008b33fa36a8e
R4mTex <poutots@gmail.com>
2023-11-01 15:26:49 +0100

    update travis

--- toolshare/authentication/tests/test_urls.py
+++ toolshare/authentication/tests/test_urls.py
@@ -1,16 +1,16 @@
 import pytest
 from django.urls import reverse, resolve
-from authentication import models
+from authentication.models import User
 
 
 # Create your tests here.
 @pytest.mark.django_db
 def test_about_url():
-    models.User.objects.create(username='Test User',
-                               email='',
-                               password='',
-                               )
+    User.objects.create(username='Test User',
+                        email='',
+                        password='',
+                        )
     path = reverse('about', kwargs={'userID': User.objects.all()[0].id})
 
-    assert path == "/user/1/about/"
+    assert path == '/user/' + str(User.objects.all()[0].id) + '/about/'
     assert resolve(path).view_name == "about"
